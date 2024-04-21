72f11876a14b985ebe87f83737dc217782d1224d
InamotnecM <145865527+morenacentomani@users.noreply.github.com>
2024-04-21 17:03:15 -0300

    Subo Etapa 0

---
+++ Etapa 0/1_Centomani_HolaNombre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Centomani_HolaNombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            Console.WriteLine("Ingrese su nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.ReadKey();
+        }
+    }
+}
