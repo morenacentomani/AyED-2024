47eb380b1f60711b13780218e09bfdd2d2f62a61
InamotnecM <145865527+morenacentomani@users.noreply.github.com>
2024-05-11 12:32:55 -0300

    Subo ejercicio 0 y 1 de la etapa 2

---
+++ Etapa 2/0_Centomani_LaCarreraDeHomero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Centomani_LaCarreraDeHomero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de obstáculos que Homero Simpson debe superar : ");
+            int[] losobstaculos = new int[int.Parse(Console.ReadLine())];
+
+            string obssup;
+            int puntos = 0; 
+
+            for (int i = 0; i < losobstaculos.Count(); i++)
+            {
+
+                Console.Write("Obstáculo " + i + " ¿Lo supera?");
+
+                obssup = Console.ReadLine();
+
+
+                if (obssup == "si")
+                {
+
+                    losobstaculos[i] = 10;
+
+                }
+                else if (obssup == "no")
+                {
+                    
+                    losobstaculos[i] = -5;
+                }
+
+            }
+            for (int j = 0; j < losobstaculos.Count(); j++)
+            {
+
+                puntos = losobstaculos[j] + puntos;
+                Console.WriteLine(puntos);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/1_Centomani_LaFiestaDeStitch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Centomani_LaFiestaDeStitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuántos invitados van a venir?");
+            int[] losinvitados = new int[int.Parse(Console.ReadLine())];
+
+            int comida = 0;
+            
+            for (int i = 0; i < losinvitados.Count(); i++)
+            {
+                Console.Write("El invitado " + i + " come: ");
+                comida = int.Parse(Console.ReadLine());
+
+                while (comida > 100 || comida < 1)
+                {
+                    Console.WriteLine("error");
+                    Console.Write("El invitado " + i + " come: ");
+                    comida = int.Parse(Console.ReadLine());
+                }
+
+                if (comida <= 100 && comida >= 1)
+                {
+
+                    losinvitados[i] = comida;
+
+                }
+
+            }
+            comida = 0;
+            for (int j = 0; j < losinvitados.Count(); j++)
+            {
+                comida = losinvitados[j] + comida;
+
+            }
+            Console.WriteLine("Este es el promedio: " + (comida / losinvitados.Count()));
+
+            Console.ReadKey();
+        }
+    }
+}
