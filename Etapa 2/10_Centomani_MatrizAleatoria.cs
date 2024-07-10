4752e95382b23ac1c8ec2a6757432b0512fcdc02
InamotnecM <145865527+morenacentomani@users.noreply.github.com>
2024-07-10 15:22:51 -0300

    Subo del 8 al 11

---
+++ Etapa 2/10_Centomani_MatrizAleatoria.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Centomani_MatrizAleatoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            Console.Write("Filas: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Columnas: ");
+            m = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, m];
+
+            Random rango = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[i, j] = rango.Next(1, 10);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/11_Centomani_SumandoMatrices.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Centomani_SumandoMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Definí el rango de filas y columnas de tu matriz (solo un número): ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] matriz1 = new int[n, n];
+            int[,] matriz2 = new int[n, n];
+            int[,] matriz3 = new int[n, n];
+
+            Random rango = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz1[i, j] = rango.Next(1, 10);
+                    matriz2[i, j] = rango.Next(1, 10);
+                    matriz3[i, j] = matriz1[i, j] + matriz2[i, j];
+                }
+            }
+
+            Console.WriteLine("Matriz 1");
+            Console.WriteLine("-------------");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz1[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Matriz 2");
+            Console.WriteLine("-------------");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz2[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Matrices sumadas");
+            Console.WriteLine("-------------");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matriz3[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
