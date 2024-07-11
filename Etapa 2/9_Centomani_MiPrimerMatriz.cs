005635c0caaed1a05e08ad1381512a62107feba1
InamotnecM <145865527+morenacentomani@users.noreply.github.com>
2024-07-11 13:12:18 -0300

    Subo el 9

---
+++ Etapa 2/9_Centomani_MiPrimerMatriz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Centomani_MiPrimerMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m, num = 0;
+
+
+            Console.Write("Filas: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Columnas: ");
+            m = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, m];
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matriz[i, j] = num;
+                    num++;
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matriz[i,j] + "\t");
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
