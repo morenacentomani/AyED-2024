36c46074811f5e148cdc18797e7b209e1e89337e
InamotnecM <145865527+morenacentomani@users.noreply.github.com>
2024-08-14 14:35:50 -0300

    subo tp 0

---
+++ Etapa 3/0_Centomani_MiPrimerFuncion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Centomani_MiPrimerFuncion
+{
+    class Program
+    {
+        static string saludar(string nom)
+        {
+            string saludo = ("Hola " + nom);
+            return saludo;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(saludar("More"));
+
+            Console.ReadKey();
+        }
+    }
+}
